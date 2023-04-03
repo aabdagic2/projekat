88bcef7e2c2525f89140331b27c62d85eb59d1d6
aminaabdagic <92653857+aabdagic2@users.noreply.github.com>
2023-04-03 17:51:21 +0200

    Spirala 1

---
+++ vjezba/app/src/main/java/ba/etf/rma22/vjezba/Anketa.kt
@@ -0,0 +1,3 @@
+package ba.etf.rma22.vjezba
+
+data class Anketa (var naziv:String,var brojPitanja:Int,var redniBroj:Int)
---
+++ vjezba/app/src/main/java/ba/etf/rma22/vjezba/Pitanje.kt
@@ -0,0 +1,3 @@
+package ba.etf.rma22.vjezba
+
+data class Pitanje(var tekstPitanja:String, var odgovor:String?,var anketa:Int)
